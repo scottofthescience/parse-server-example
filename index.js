16df2d2d80719cbbd5b803a7788dca77db7cd712
Scott Allen Cambo <scottcambo2019@u.northwestern.edu>
2016-06-21 14:46:21 -0700

    added dashboard?

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
+var ParseDashboard = require('parse-dashboard');
 var path = require('path');
 
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
@@ -25,6 +26,17 @@ var api = new ParseServer({
 // If you wish you require them, you can set them as options in the initialization above:
 // javascriptKey, restAPIKey, dotNetKey, clientKey
 
+var dashboard = new ParseDashboard(
+  {
+  "apps": [
+    {
+      "serverURL": "https://msbandcollectertest.herokuapp.com/parse",
+      "appId": "myAppId",
+      "masterKey": "",
+      "appName":""
+    }
+  ]
+});
 var app = express();
 
 // Serve static assets from the /public folder
@@ -33,6 +45,9 @@ app.use('/public', express.static(path.join(__dirname, '/public')));
 // Serve the Parse API on the /parse URL prefix
 var mountPath = process.env.PARSE_MOUNT || '/parse';
 app.use(mountPath, api);
+
+// make the Parse Dashboard available at /dashboard
+app.use('/dashboard', dashboard);
 
 // Parse Server plays nicely with the rest of your web routes
 app.get('/', function(req, res) {
